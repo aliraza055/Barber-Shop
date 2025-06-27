b84c21c50c248ac483f5a9cb990ab9f8fb874272
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-06-27 22:19:45 +0500

    create splash screen

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -10,6 +10,48 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color:  Color(0xFF2B1B17),
+        child: Column(
+          children: [ 
+            Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: Image.asset("assets/barber.png",),
+            ),
+            SizedBox(height: 50,),
+           GestureDetector(
+  onTap: () {
+    print("Button tapped!");
+  },
+  child: Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    decoration: BoxDecoration(
+      color: Colors.orange,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 6,
+          offset: Offset(0, 4),
+        ),
+      ],
+    ),
+    child: Text(
+      "Get a Stylish Haircut",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 1,
+      ),
+    ),
+  ),
+)
+
+          ],
+        ),
+      ),
+    );
   }
 }
