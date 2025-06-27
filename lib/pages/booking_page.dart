6ea6d51fe56c923fbe10a22855ce68132aba0637
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-06-27 23:29:42 +0500

    add images in booking page

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -11,11 +11,19 @@ class _BookingPageState extends State<BookingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+              backgroundColor:  Color(0xFF2B1B17),
+
      body: Container(
-      margin: EdgeInsets.only(top: 30,left: 20),
+      margin: EdgeInsets.only(top: 30,left: 20,right: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Lets the \n Journey Begins",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w400),)
+          Text("Lets the \nJourney Begins!",
+          style: TextStyle(
+            color: Colors.white60,fontSize: 30,
+            fontWeight: FontWeight.w400),),
+            SizedBox(height: 20,),
+            Image.asset('assets/dicount.png',height: 100,width: double.infinity,fit: BoxFit.fill,)
         ],
       ),
      ),
