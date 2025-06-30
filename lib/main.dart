c5a32b84257781973ae389b3b3f78d21ceb99bb8
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-06-30 18:33:43 +0500

    add firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,23 @@
 import 'package:barber_shop/pages/sign_up.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-      
-       
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const SignUp()
     );
