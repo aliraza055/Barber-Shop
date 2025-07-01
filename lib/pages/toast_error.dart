82f1a5704bd33379681bd7f3058ea4b017858193
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-01 22:39:19 +0500

    add required to parameters

--- lib/pages/toast_error.dart
+++ lib/pages/toast_error.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 class ToastError{
-  void showToast(String msg,Color color,Color textColor){
+  void showToast({required String msg,required Color color,required Color textColor}){
     Fluttertoast.showToast(
       msg: msg,
       backgroundColor: color,
       toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.CENTER,
         timeInSecForIosWeb: 1,
-        textColor: textColor
+        textColor: textColor,
+                fontSize: 16.0,
+
       );
   }
 }
