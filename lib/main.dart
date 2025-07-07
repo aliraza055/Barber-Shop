800442c5da8a7b350aec7c0d5f374d3f97265ede
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-07 21:24:47 +0500

    remove bogus url

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:barber_shop/pages/sendUi.dart';
-import 'package:barber_shop/pages/view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
