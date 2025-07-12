5b3553c132c0721db7b03d081d82df855e5f28b5
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-12 21:34:34 +0500

    add name preferences

--- lib/model/shared_preferece.dart
+++ lib/model/shared_preferece.dart
@@ -1,3 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPreferece {
-  
+  final  String nameKey="NAME";
+  Future sendName(String name)async{
+  SharedPreferences pref=await SharedPreferences.getInstance();
+  return pref.setString(nameKey, name);
+  }
 }
