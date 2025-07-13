f270609284d0f944d015ef161a87dc16d3a4842e
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-14 00:10:23 +0500

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:barber_shop/pages/admin_order.dart';
-import 'package:barber_shop/pages/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
--- lib/pages/admin_order.dart
+++ lib/pages/admin_order.dart
@@ -17,20 +17,24 @@ class _AdminOrderState extends State<AdminOrder> {
         title: Text("Orders"),
       ),
       body: Container(
-        color: Colors.black12,
-        margin: EdgeInsets.only(left: 30,right: 30,top: 20),
-        child: Column(children: [
-          Container(
-            height: 60,
-            color: Colors.white,
-            child: Row(
-              children: [
-                Image.asset('assets/cutting.png'),
-
-              ],
-            ),
-          )
-        ],),
+      color: Colors.black12,
+        child: Container(
+          margin: EdgeInsets.only(left: 30,right: 30,top: 20),
+          child: Column(children: [
+            Container(
+            padding: EdgeInsets.all(8),
+              height: 60,
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/cutting.png',width: 50,height: 50,),
+                  ],
+                ),
+              ),
+            )
+          ],),
+        ),
       ),
     );
   }
