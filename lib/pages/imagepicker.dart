a7d75ef4712b5393084e250acde1232d9ca86ea4
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-20 17:11:14 +0500

    uplaod image to cloudnary

--- lib/pages/imagepicker.dart
+++ lib/pages/imagepicker.dart
@@ -1,8 +1,12 @@
 
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:barber_shop/pages/toast_error.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class Imagepicker extends StatefulWidget {
   const Imagepicker({super.key});
@@ -25,7 +29,7 @@ class _ImagepickerState extends State<Imagepicker> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              image == null ? Text("No image selected") : Image.file(image!),
+              image == null ? Text("No image selected") : Image.file(image!,height:150,width: double.infinity,),
               SizedBox(height: 20,),
               
               FloatingActionButton(
@@ -49,7 +53,26 @@ class _ImagepickerState extends State<Imagepicker> {
                   padding: const EdgeInsets.all(8.0),
                   child: Text("upload image to cloudinary"),
                 ),
-                onPressed: (){}),
+                onPressed: ()async{
+                  final url=Uri.parse('https://api.cloudinary.com/v1_1/dhob4di7g/image/upload');
+                 final request=http.MultipartRequest("POST", url);
+                 request.fields["upload_preset"]='upload_preset_file';
+                 request.files.add(await http.MultipartFile.fromPath('file', image!.path));
+                 final response = await request.send();
+
+if (response.statusCode == 200) {
+  final responseBody = await response.stream.bytesToString();
+final decoded = json.decode(responseBody);
+final imageUrl = decoded["secure_url"];  
+ 
+
+  ToastError().showToast(msg: '✅ Upload successful: $responseBody',color: Colors.green,textColor: Colors.white);
+} else {
+    ToastError().showToast(msg: '❌ Upload failed: ${response.statusCode}',color: Colors.green,textColor: Colors.white);
+
+}
+
+                }),
             ],
           ),
         ),
