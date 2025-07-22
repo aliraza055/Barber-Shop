37db0120afa8934c76221bebed8528e5cde869b9
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-22 20:08:29 +0500

    add in into main page

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   const Upload({super.key});
@@ -8,8 +11,35 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+File? image;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          image == null ? Text("no image selected") : Image.file(image!),
+          FloatingActionButton(onPressed: (){
+
+          },
+          child: Text("select"),),
+          SizedBox(height: 20,),
+             FloatingActionButton(onPressed: (){
+
+          },
+          child: Text("select"),)
+        ],
+      ),
+    );
+  }
+  Future selectImage()async{
+  final  picker=ImagePicker();
+final  slectedImage=await picker.pickImage(source: ImageSource.gallery);
+if(slectedImage != null){
+  image=File(slectedImage.path);
+  setState(() {
+    
+  });
+}
+
   }
 }
