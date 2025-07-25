b92c74874f9cf01f97fa825ffe43290dfd13be24
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-25 21:57:37 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:barber_shop/pages/upload.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
    await Firebase.initializeApp();
