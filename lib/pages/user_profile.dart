b60653c25122ce575175b146f9357d4d95017ec9
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-07-27 00:10:48 +0500

    edit userprofile

--- lib/pages/user_profile.dart
+++ lib/pages/user_profile.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:barber_shop/pages/toast_error.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class UserProfile extends StatefulWidget {
   const UserProfile({super.key});
@@ -8,25 +16,56 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
+      File? _image;
+      User? user=FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _image == null ? Text("No image selected!") : Image.file(_image!,height: 200,width: 200,),
+                      SizedBox(height: 20,),
+
+          FloatingActionButton(
+            child: Text("get"),
+            onPressed: ()async{
+          final currentImage=await ImagePicker().pickImage(source: ImageSource.gallery);
+          if(currentImage != null){
+            _image=File(currentImage.path);
+            setState(() {
+              
+            });
+
+          }
+        
+          }),
+          SizedBox(height: 20,),
         FloatingActionButton(
-          child: Text("get"),
-          onPressed: (){
+            child: Text("upload"),
+            onPressed: ()async{
+          final uri=Uri.parse('https://api.cloudinary.com/v1_1/dhob4di7g/image/upload');
+          final request=http.MultipartRequest('POST', uri);
+          request.fields['upload_present']='upload_preset_file';
+          request.files.add(await http.MultipartFile.fromPath('file', _image!.path));
+      final resposne=  await request.send();
+      if(resposne.statusCode==200){
+        final resBody=await resposne.stream.bytesToString();
+        final decoede=jsonDecode(resBody);
+        final imageurl=decoede['secure_url'];
+        user!.updatePhotoURL(imageurl);
+        await user!.reload();
+      await FirebaseFirestore.instance.collection("Users").doc(user!.uid).update({
+        'image':imageurl
+      });
+          ToastError().showToast(msg: "Uploaded Successfully!", color: Colors.black, textColor: Colors.white);
+      }
 
-        }),
-        SizedBox(height: 20,),
-FloatingActionButton(
-          child: Text("upload"),
-          onPressed: (){
-
-        }),
-        ],
+          }),
+          ],
+        ),
       ),
     );
   }
