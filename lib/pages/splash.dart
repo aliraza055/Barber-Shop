eb99f372452a5b1cdf64d15762760e2e82510f56
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-03 19:58:32 +0500

    change the splash sreen

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -12,44 +12,47 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color:  Colors.white,
+      backgroundColor: Colors.green.shade900,
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [ 
-            Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Image.asset("assets/barber.png",),
-            ),                      
+            Image.asset("assets/cutting.png",height: 200,width: 200,), 
+             Text('Welcome to',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),                     
+            Text(' SallonX',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),                   
             SizedBox(height: 50,),
            GestureDetector
            (
-  onTap: () {
-    Navigator.of(context).push(MaterialPageRoute(builder: (_)=>SignUp()));
-  },
-  child: Container(
-    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-    decoration: BoxDecoration(
-      color: Colors.orange,
-      borderRadius: BorderRadius.circular(12),
-      boxShadow: [
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (_)=>SignUp()));
+          },
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                    colors: [Colors.deepPurple, Colors.indigo], // HomePage gradient
+                  ),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
         BoxShadow(
           color: Colors.black26,
           blurRadius: 6,
           offset: Offset(0, 4),
         ),
-      ],
-    ),
-    child: Text(
-      "Get a Stylish Haircut",
-      style: TextStyle(
+              ],
+            ),
+            child: Text(
+              "Get a Stylish Haircut",
+              style: TextStyle(
         color: Colors.white,
         fontSize: 16,
         fontWeight: FontWeight.bold,
         letterSpacing: 1,
-      ),
-    ),
-  ),
-)
+              ),
+            ),
+          ),
+        )
           ],
         ),
       ),
