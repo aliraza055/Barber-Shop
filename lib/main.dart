06ed7e1b92563d454815ef7d7c066afca17a0753
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-04 21:47:36 +0500

    Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,8 @@ void main() async {
    await Firebase.initializeApp();
    runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
